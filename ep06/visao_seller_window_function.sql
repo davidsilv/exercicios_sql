4595255f314f028286cc98a1fa345f71efb2fb5d
davidsilv <davidpereirax19@gmail.com>
2021-12-27 19:28:55 -0300

    ep06 adicionado

---
+++ ep06/visao_seller_window_function.sql
@@ -0,0 +1,35 @@
+-- qual o produto campeão de vendas de cada seller
+-- considere apenas pedidos entregues.
+
+with tb_seller_product as (
+    select t2.seller_id, 
+            product_id, 
+            count(*) as qtde_produto, 
+            sum(price) as receita_produto 
+
+    from tb_orders as t1
+
+    left join tb_order_items as t2
+    on t1.order_id = t2.order_id
+
+    where order_status = 'delivered'
+
+    group by t2.seller_id,
+                t2.product_id
+    order by seller_id
+),
+
+tb_seller_sort as (
+select *, 
+        row_number() over ( partition by seller_id order by qtde_produto desc, receita_produto desc) as order_qtde
+
+from tb_seller_product
+)
+
+select seller_id,
+        product_id,
+        qtde_produto
+
+from tb_seller_sort
+
+where order_qtde = 1
